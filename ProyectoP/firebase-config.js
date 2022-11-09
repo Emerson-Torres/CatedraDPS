13c55f4009223e5c503536539072972e5887eb70
Emerson-Torres <30862610+Emerson-Torres@users.noreply.github.com>
2022-11-09 17:05:26 -0600

    fghfrgh

---
+++ ProyectoP/firebase-config.js
@@ -0,0 +1,17 @@
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyBcDVGeC6Daohrvs04q6xHvOyoyiZ4wxBE",
+    authDomain: "dpscatedra-2ea14.firebaseapp.com",
+    projectId: "dpscatedra-2ea14",
+    storageBucket: "dpscatedra-2ea14.appspot.com",
+    messagingSenderId: "301946366788",
+    appId: "1:301946366788:web:2c57053523fc8e0a828511",
+    measurementId: "G-45KG4NLSSS"
+  };
+  
+  
